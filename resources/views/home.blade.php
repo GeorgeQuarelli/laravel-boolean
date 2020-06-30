6c4e190c9f32b860d5fc24e08b373525fff1c8b3
George <quarelli.george@gmail.com>
2020-06-30 22:27:19 +0200

    footer parte 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,19 +14,75 @@
 
 {{-- main --}}
 <main>
+
+    {{-- section1 --}}
     <section class="container">
-        @include('partials.section1')
+        <div id="section1">
+
+            <div class="main_left">
+                <h3>DIVENTA</h3>
+                <h2>SVILUPPATORE WEB</h2>
+                <p>Trasformiamo la tua passione in una carriera. Se non <br> trovi lavoro, ti rimborsiamo.</p>
+                <ul>
+                    <li><strong>6 mesi</strong> di corso intensivo online in diretta</li>
+                    <li><strong>Nessuna competenza</strong> di programmazione richiesta</li>
+                    <li>Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong></li>
+                </ul>
+            </div>
+            <div class="main_right">
+                <img src="{{asset ('img/pc-black-gif.gif')}}" alt="pc_code">
+            </div>
+        </div>
     </section>
+    {{-- fine section1 --}}
+
+    {{-- section2 --}}
     <section id="section2">
-        @include('partials.section2')
+        <section class="container">
+
+            <div class="statics">
+                <div class="box1">
+                    <h2>98%</h2>
+                    <p>La percentuale dei nostri <br> studenti che ora lavora come <br> web developer, oltre la metà a <br> tempo indeterminato.</p>
+                </div>
+                <div class="box2">
+                    <h2>€ 23.000</h2>
+                    <p>Lo stipendio medio lordo di <br> partenza degli studenti assunti <br> dalle nostre aziende partner.</p>
+                </div>
+                <div class="box1">
+                    <h2>#1</h2>
+                    <p>Siamo il primo istituto online in <br> Italia per indice di gradimento e <br> risultati conseguiti.</p>
+                </div>
+            </div>
+        </section>
     </section>
+    {{-- fine section2 --}}
+
+    {{-- section3  --}}
+    <section class='container'>
+
+            <span class="text">
+                <p>I nostri studenti sono stati assunti da:</p>
+            </span>
+            <div class="loghi_aziende1">
+                <img src="{{asset ('img/facileit.png')}}" alt="facile.it">
+                <img class="img_center" src="{{asset ('img/oneday.png')}}" alt="oneday">
+                <img src="{{asset ('img/prima.png')}}" alt="prima">
+            </div>
+            <div class="loghi_aziende2">
+                <img src="{{asset ('img/fabbrica-digitale-big.png')}}" alt="fabbrica-digitale">
+                <img class="img_center" src="{{asset ('img/nephila.png')}}" alt="nephila">
+                <img src="{{asset ('img/rds.png')}}" alt="rds">
+            </div>
+
+    </section>
+    {{-- fine section3 --}}
 
 </main>
 
 {{-- fine main --}}
-
-<footer>
-
-</footer>
+{{-- inizio footer --}}
+    @include('partials.footer')
+{{-- fine footer --}}
     </body>
 </html>
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,31 @@
+<footer>
+
+    <div id="footer">
+        <div class="container">
+            <div class="footer_1">
+                <div class="footer_left">
+                    <img src="{{asset ('img/logo-white.png')}}" alt="">
+                    <small>Sede Operativa</small>
+                    <small>Via Carducci 12 - 20123 Milano</small>
+                    <small>Tel: 02-40031288</small>
+                </div>
+                <div class="footer_right">
+                    <p> <a href="#"> Lezione Gratuita </a> </p>
+                    <p> <a href="#"> Domande frequenti </a> </p>
+                    <p> <a href="#"> Privacy policy </a> </p>
+                    <p> <a href="#"> Lavora con noi </a> </p>
+                </div>
+            </div>
+            <div class="footer_2">
+                <div class="loghi_social">
+
+                </div>
+                <div class="adress">
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</footer>
