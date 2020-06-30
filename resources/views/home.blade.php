7889d9c10bad9c96e06baa72f91d5338556b5a10
George <quarelli.george@gmail.com>
2020-06-30 15:20:47 +0200

    inizio menu

---
+++ resources/views/home.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+    </head>
+    <body>
+{{-- menu  --}}
+        <header>
+            <div class="container_header">
+                <div class="header_left">
+                    <img src="" alt="">
+                </div>
+                <div class="header_right">
+                    <ul>
+                        <li> <a href="#"> HOME </a> </li>
+                        <li> <a href="#"> CORSO </a> </li>
+                        <li> <a href="#"> DOPO IL CORSO </a> </li>
+                        <li> <a href="#"> LEZIONE GRATUITA </a> </li>
+                        <li> <a href="#"> ASSUMI I NOSTRI STUDENTI </a> </li>
+                    </ul>
+                    <button type="button" name="button"> <a href="#"> CANDIDATI ORA </a></button>
+                </div>
+            </div>
+        </header>
+{{-- fine menu --}}
+    </body>
+</html>
